9ba4acaa32324bc19ccbeae057bfa963b6a3658e
Daksh-Parekh <dakshparekh@gmail.com>
2024-08-23 14:55:07 +0530

    Date picker

--- lib/utils/globals.dart
+++ lib/utils/globals.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 class Globals {
-  static String? name, email, contact, address, password, gender;
+  static String? name, email, contact, address, password, gender, dob;
   static File? image;
   static bool isChecked = false;
   static List<String> languages = [];
--- lib/views/buildoption/all_option/personal_detail_page/personal_detail.dart
+++ lib/views/buildoption/all_option/personal_detail_page/personal_detail.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:resume_bulider/utils/extension.dart';
 import 'package:resume_bulider/utils/globals.dart';
 
 class PersonalDetails extends StatefulWidget {
@@ -14,6 +17,9 @@ class _PersonalDetailsState extends State<PersonalDetails> {
     'Gujarati',
     'Hindi',
   ];
+  TextEditingController dateController =
+      TextEditingController(text: Globals.dob);
+  GlobalKey<FormState> FormKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,76 +33,124 @@ class _PersonalDetailsState extends State<PersonalDetails> {
           width: double.infinity,
           padding: EdgeInsets.all(16),
           color: Colors.white,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text(
-                "Select your Gender",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text("Date of Birth"),
+                Form(
+                  key: FormKey,
+                  child: TextFormField(
+                    controller: dateController,
+                    readOnly: true,
+                    onSaved: (val) {
+                      Globals.dob = val;
+                    },
+                    // onChanged: (value) {
+                    //   dateController.text = value;
+                    //   Globals.dob = dateController.text;
+                    //   setState(() {});
+                    // },
+                    onTap: () async {
+                      DateTime? datepicked = await showDatePicker(
+                        context: context,
+                        firstDate: DateTime(1998),
+                        lastDate: DateTime.now(),
+                      );
+                      if (datepicked != null) {
+                        dateController.text =
+                            datepicked.toString().split(" ")[0];
+                        setState(() {});
+                      }
+                      // if (datepicked != null) {
+                      //   Globals.dob =
+                      //       datepicked.toString().toString().split(" ")[0];
+                      // }
+                    },
+                    decoration: InputDecoration(
+                      hintText: "DD/MM/YYYY",
+                    ),
+                  ),
                 ),
-              ),
-              RadioListTile(
-                title: Text("Male"),
-                value: "Male",
-                groupValue: Globals.gender,
-                onChanged: (val) {
-                  Globals.gender = val;
-                  setState(() {});
-                },
-              ),
-              RadioListTile(
-                title: Text("Female"),
-                value: "Female",
-                groupValue: Globals.gender,
-                onChanged: (val) {
-                  Globals.gender = val;
-                  setState(() {});
-                },
-              ),
-              RadioListTile(
-                title: Text("Others"),
-                value: "Others",
-                groupValue: Globals.gender,
-                onChanged: (val) {
-                  Globals.gender = val;
-                  setState(() {});
-                },
-              ),
-              Text(
-                "Languages",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18,
+                10.h,
+                Text(
+                  "Select your Gender",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
                 ),
-              ),
-              // Checkbox(
-              //   value: Globals.isChecked,
-              //   onChanged: (val) {
-              //     Globals.isChecked = val ?? false;
-              //     setState(() {});
-              //   },
-              // ),
-
-              ...availableLanguages.map(
-                (e) => CheckboxListTile(
-                  title: Text(e),
-                  controlAffinity: ListTileControlAffinity.leading,
-                  value: Globals.languages.contains(e),
+                RadioListTile(
+                  title: Text("Male"),
+                  value: "Male",
+                  groupValue: Globals.gender,
                   onChanged: (val) {
-                    if (val!) {
-                      Globals.languages.add(e);
-                    } else {
-                      Globals.languages.remove(e);
-                    }
+                    Globals.gender = val;
                     setState(() {});
-                    // Globals.isChecked = val ?? false;
                   },
                 ),
-              ),
-            ],
+                RadioListTile(
+                  title: Text("Female"),
+                  value: "Female",
+                  groupValue: Globals.gender,
+                  onChanged: (val) {
+                    Globals.gender = val;
+                    setState(() {});
+                  },
+                ),
+                RadioListTile(
+                  title: Text("Others"),
+                  value: "Others",
+                  groupValue: Globals.gender,
+                  onChanged: (val) {
+                    Globals.gender = val;
+                    setState(() {});
+                  },
+                ),
+                Text(
+                  "Languages",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+                // Checkbox(
+                //   value: Globals.isChecked,
+                //   onChanged: (val) {
+                //     Globals.isChecked = val ?? false;
+                //     setState(() {});
+                //   },
+                // ),
+
+                ...availableLanguages.map(
+                  (e) => CheckboxListTile(
+                    title: Text(e),
+                    controlAffinity: ListTileControlAffinity.leading,
+                    value: Globals.languages.contains(e),
+                    onChanged: (val) {
+                      if (val!) {
+                        Globals.languages.add(e);
+                      } else {
+                        Globals.languages.remove(e);
+                      }
+                      setState(() {});
+                      // Globals.isChecked = val ?? false;
+                    },
+                  ),
+                ),
+
+                ElevatedButton(
+                  onPressed: () {
+                    bool validated = FormKey.currentState!.validate();
+                    if (validated) {
+                      FormKey.currentState!.save();
+                    }
+                  },
+                  child: Text("Save"),
+                ),
+              ],
+            ),
           ),
         ),
       ),
